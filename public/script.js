d7eb35362904084beb3d97b754e91e7ef60880f1
karinmcode <karinmorandell.pro@gmail.com>
2024-01-29 13:05:46 +0100

    removed public folder

--- public/script.js
+++
@@ -1,6 +0,0 @@
-fetch('https://script.google.com/macros/s/AKfycbz7jCwb-JcH30BgwxjR85iZYwdGurcxaJZql7sh6yAxQQTcg7c_cK8rxViY-5TVsplo-g/exec')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Process and display your data here
-  })
-  .catch(error => console.error('Error:', error));
